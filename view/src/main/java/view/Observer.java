d7bca35c4c153ee1cf635300f8d874f3e9191bfc
Eldackar <guillaume.burgeot@viacesi.fr>
2016-06-20 17:35:11 +0200

    Add files via upload

---
+++ view/src/main/java/view/Observer.java
@@ -0,0 +1,13 @@
+package view;
+
+/**
+ * 
+ *	interface used to build an observer patern
+ */
+public interface Observer 
+{
+	/**
+	 * 	methode used to update information sent to a class
+	 */
+	public void update();
+}
